cf38b2c437028d012be732ebcff1a11d083de3e6
Yaroslav <ia.king666@yandex.ru>
2024-12-01 00:00:59 +0300

    second

--- src/components/table/TableComponent.js
+++ src/components/table/TableComponent.js
@@ -6,8 +6,8 @@ const TableComponent = () => {
     const dispatch = useDispatch();
     const posts = useSelector((state) => state.posts.posts);
     const postStatus = useSelector((state) => state.posts.status);
-
     const [currentPage, setCurrentPage] = useState(1);
+    const [filterId, setFilterId] = useState('');
     const postsPerPage = 10;
 
     useEffect(() => {
@@ -16,56 +16,76 @@ const TableComponent = () => {
         }
     }, [postStatus, dispatch]);
 
+    // Фильтруем посты по ID
+    const filteredPosts = filterId 
+        ? posts.filter(post => post.id.toString() === filterId)
+        : posts;
+
     // Логика для пагинации
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-    const totalPages = Math.ceil(posts.length / postsPerPage);
+    const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
+    const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
 
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
 
-const tableStyle = {
-      width: '100%',
-      borderCollapse: 'collapse'
-  };
+    const handleFilterChange = (event) => {
+        const id = event.target.value;
+        setFilterId(id);
+        setCurrentPage(1); // Сбросить текущую страницу при изменении фильтра
+    };
   
-const cellStyle = {
- padding: '15px',
- border: '1px solid #ccc',
- textAlign: 'left'
-};
+    const tableStyle = {
+        width: '100%',
+        borderCollapse: 'collapse',
+    };
   
-const headerStyle = {
- ...cellStyle,
- backgroundColor: '#f2f2f2'    
-};
+    const cellStyle = {
+        padding: '15px',
+        border: '1px solid #ccc',
+        textAlign: 'left',
+    };
+  
+    const headerStyle = {
+        ...cellStyle,
+        backgroundColor: '#f2f2f2',
+    };
 
-const buttonStyle = {
-  margin: '0 5px',
-  padding: '10px 15px',
-  border: '1px solid #ccc',
-  backgroundColor: '#f2f2f2',
-  cursor: 'pointer',
-  borderRadius: '4px'
-};  
+    const buttonStyle = {
+        margin: '0 5px',
+        padding: '10px 15px',
+        border: '1px solid #ccc',
+        backgroundColor: '#f2f2f2',
+        cursor: 'pointer',
+        borderRadius: '4px',
+    };
 
     return (
         <div>
             <h2>Посты</h2>
+
+            <input
+                style={{ padding: '5px', margin: '5px' }}
+                type='number'
+                value={filterId}
+                onChange={handleFilterChange}
+                placeholder='Фильтр постов по ID'
+            />
+            <h3>Результаты поиска:</h3>
+            
             <table style={tableStyle}>
                 <thead>
                     <tr>
-                        <th style={headerStyle}>id</th>
-                        <th style={headerStyle}>title</th>
-                        <th style={headerStyle}>body</th>
+                        <th style={headerStyle}>ID</th>
+                        <th style={headerStyle}>Заголовок</th>
+                        <th style={headerStyle}>Содержимое</th>
                     </tr>
                 </thead>
                 <tbody>
                     {currentPosts.length > 0 ? (
-                        currentPosts.map((post) => (
+                        currentPosts.map(post => (
                             <tr key={post.id}>
                                 <td>{post.id}</td>
                                 <td>{post.title}</td>
@@ -74,15 +94,19 @@ const buttonStyle = {
                         ))
                     ) : (
                         <tr>
-                            <td colSpan="3">Загружаются посты...</td>
+                            <td colSpan="3">Посты не найдены.</td>
                         </tr>
                     )}
                 </tbody>
             </table>
             <div>
                 {/* Пагинация */}
-                {Array.from({ length: totalPages }, (_, index) => (
-                    <button style={buttonStyle} key={index} onClick={() => handlePageChange(index + 1)} disabled={currentPage === index + 1}>
+                {totalPages > 1 && Array.from({ length: totalPages }, (_, index) => (
+                    <button
+                        style={buttonStyle}
+                        key={index}
+                        onClick={() => handlePageChange(index + 1)}
+                        disabled={currentPage === index + 1}>
                         {index + 1}
                     </button>
                 ))}
